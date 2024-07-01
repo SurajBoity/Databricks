5d7bbadd0f025a251e4aee03530956e5584309fc
SurajBoity <142211781+SurajBoity@users.noreply.github.com>
2024-07-01 17:02:38 +0530

    Update: .vs IPL.ipynb README.md notebooks surajkumarboity@gmail.com.zip

--- notebooks/Test/App_airline.py
+++ notebooks/Test/App_airline.py
@@ -1,6 +1,11 @@
 # Databricks notebook source
+dbutils.fs.rm('dbfs:/user/hive/warehouse/app_airline', True)
+
+# COMMAND ----------
+
 # MAGIC %sql
-# MAGIC CREATE TABLE App_Airline(
+# MAGIC --DROP TABLE IF EXISTS  hive_metastore.app_Airline;
+# MAGIC CREATE OR REPLACE TABLE app_Airline(
 # MAGIC 	Unique_ID STRING,
 # MAGIC 	Date_Created TIMESTAMP ,
 # MAGIC 	Date_Modified TIMESTAMP,
@@ -44,7 +49,21 @@ df.write.mode('overwrite').insertInto('App_Airline')
 # COMMAND ----------
 
 # MAGIC %sql
+# MAGIC CACHE SELECT * FROM App_Airline
+
+# COMMAND ----------
+
+# MAGIC %sql
 # MAGIC SELECT * FROM App_Airline
 
 # COMMAND ----------
 
+df = spark.createDataFrame([
+    ['red', 'banana', 1, 10], ['blue', 'banana', 2, 20], ['red', 'carrot', 3, 30],
+    ['blue', 'grape', 4, 40], ['red', 'carrot', 5, 50], ['black', 'carrot', 6, 60],
+    ['red', 'banana', 7, 70], ['red', 'grape', 8, 80]], schema=['color', 'fruit', 'v1', 'v2'])
+df.show()
+
+df.groupby('color').avg().show()
+
+df.dtypes
